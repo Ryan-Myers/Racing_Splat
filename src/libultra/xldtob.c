51a4152f9d203c0ebc2cd7511a881430e0c4f4ee
Ryan Myers <ryan.p.myers@gmail.com>
2023-05-12 15:06:01 -0400

    Basic work on splitting us v1

---
+++ src/libultra/xldtob.c
@@ -0,0 +1,8 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/xldtob/_Genld.s")
+
+void func_800D6F08_D7B08(void) {
+}
+
+#pragma GLOBAL_ASM("asm/nonmatchings/libultra/xldtob/_Ldtob.s")
