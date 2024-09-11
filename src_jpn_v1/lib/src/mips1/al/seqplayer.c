11da7c2d4f023971c5ca5b4c8dae8c1506028096
Ryan Myers <ryan.p.myers@gmail.com>
2024-09-11 15:34:12 -0400

    Fix jpn and pal v2

---
+++ src_jpn_v1/lib/src/mips1/al/seqplayer.c
@@ -0,0 +1,33 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/dummy_8000A710.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__mapVoice.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__unmapVoice.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__lookupVoice.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__lookupSoundQuick.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__vsVol.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__vsDelta.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__vsPan.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__seqpReleaseVoice.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__voiceNeedsNoteKill.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__initFromBank.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__setInstChanState.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__resetPerfChanState.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__initChanState.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/__seqpStopOsc.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/mips1/al/seqplayer/set_voice_limit.s")
