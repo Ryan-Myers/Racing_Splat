620c60c5db85206e704d0c21801faab501d51129
Ryan Myers <ryan.p.myers@gmail.com>
2024-09-11 15:32:04 -0400

    Fix us v2

---
+++ src_us_v2/audio_vehicle.c
@@ -0,0 +1,25 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80004B40.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_800050D0.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80005254.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80005D08.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_800063EC.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80006AC8.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80006BFC.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80006FC8.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80007F78.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80007F88.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80007F94.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/audio_vehicle/func_80007FA4.s")
