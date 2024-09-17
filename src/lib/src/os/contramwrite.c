29e4e276fb2e04176f80edf57a67c228ae3c2439
Ryan Myers <ryan.p.myers@gmail.com>
2024-09-17 15:05:29 -0400

    contramwrite

--- src/lib/src/os/contramwrite.c
+++ src/lib/src/os/contramwrite.c
@@ -1,57 +1,145 @@
-/* The comment below is needed for this file to be picked up by generate_ld */
-/* RAM_POS: 0x800CD8F0 */
-
-#include "libultra_internal.h"
+#include "PRinternal/macros.h"
+#include "PR/os_internal.h"
+#include "PR/rcp.h"
 #include "PRinternal/controller.h"
 #include "PRinternal/siint.h"
 
+extern s32 __osPfsLastChannel;
+
+#define READFORMAT(ptr) ((__OSContRamReadFormat*)(ptr))
+
+#if BUILD_VERSION >= VERSION_J
+s32 __osContRamWrite(OSMesgQueue* mq, int channel, u16 address, u8* buffer, int force) {
+    s32 ret = 0;
+    s32 i;
+    u8* ptr;
+    s32 retry = 2;
+    u8 crc;
+
+    if ((force != TRUE) && (address < PFS_LABEL_AREA) && (address != 0)) {
+        return 0;
+    }
+
+    __osSiGetAccess();
+
+    do {
+        ptr = (u8*)__osPfsPifRam.ramarray;
+
+        if (__osContLastCmd != CONT_CMD_WRITE_PAK || (u32)__osPfsLastChannel != channel) {
+            __osContLastCmd = CONT_CMD_WRITE_PAK;
+            __osPfsLastChannel = channel;
+
+            for (i = 0; i < channel; i++) { *ptr++ = CONT_CMD_REQUEST_STATUS; }
+
+            __osPfsPifRam.pifstatus = CONT_CMD_EXE;
+
+            READFORMAT(ptr)->dummy = CONT_CMD_NOP;
+            READFORMAT(ptr)->txsize = CONT_CMD_WRITE_PAK_TX;
+            READFORMAT(ptr)->rxsize = CONT_CMD_WRITE_PAK_RX;
+            READFORMAT(ptr)->cmd = CONT_CMD_WRITE_PAK;
+            READFORMAT(ptr)->datacrc = 0xFF;
+
+            ptr[sizeof(__OSContRamReadFormat)] = CONT_CMD_END;
+        } else {
+            ptr += channel;
+        }
+
+#if BUILD_VERSION >= VERSION_J
+        READFORMAT(ptr)->addrh = address >> 3;
+        READFORMAT(ptr)->addrl = ((address << 5) | __osContAddressCrc(address));
+#else
+        READFORMAT(ptr)->address = (address << 0x5) | __osContAddressCrc(address);
+#endif
+
+        bcopy(buffer, READFORMAT(ptr)->data, BLOCKSIZE);
+
+        ret = __osSiRawStartDma(OS_WRITE, &__osPfsPifRam);
+        crc = __osContDataCrc(buffer);
+        osRecvMesg(mq, NULL, OS_MESG_BLOCK);
+
+        ret = __osSiRawStartDma(OS_READ, &__osPfsPifRam);
+        osRecvMesg(mq, NULL, OS_MESG_BLOCK);
+
+        ret = CHNL_ERR(*READFORMAT(ptr));
+
+        if (!ret) {
+            if (crc != READFORMAT(ptr)->datacrc) {
+                if ((ret = __osPfsGetStatus(mq, channel))) {
+                    break;
+                } else {
+                    ret = PFS_ERR_CONTRFAIL;
+                }
+            }
+        } else {
+            ret = PFS_ERR_NOPACK;
+        }
+    } while ((ret == PFS_ERR_CONTRFAIL) && (retry-- >= 0));
+
+    __osSiRelAccess();
+
+    return ret;
+}
+#else
+
 static void __osPackRamWriteData(int channel, u16 address, u8 *buffer);
 
-s32 __osContRamWrite(OSMesgQueue *mq, int channel, u16 address, u8 *buffer, int force) {
-    s32 ret;
-    int i;
-    u8 *ptr;
+s32 __osContRamWrite(OSMesgQueue* mq, int channel, u16 address, u8* buffer, int force) {
+    s32 ret = 0;
+    s32 i;
+    u8* ptr = (u8*)&__osPfsPifRam;
     __OSContRamReadFormat ramreadformat;
-    int retry;
+    s32 retry = 2;
+    u8 crc;
 
-    ret = 0;
-    ptr = (u8 *)&__osPfsPifRam;
-    retry = 2;
-    if (force != 1 && address < 7 && address != 0)
+    if ((force != TRUE) && (address < PFS_LABEL_AREA) && (address != 0)) {
         return 0;
+    }
+
     __osSiGetAccess();
     __osContLastCmd = CONT_CMD_WRITE_PAK;
     __osPackRamWriteData(channel, address, buffer);
     ret = __osSiRawStartDma(OS_WRITE, &__osPfsPifRam);
     osRecvMesg(mq, NULL, OS_MESG_BLOCK);
+
     do {
+#ifdef RAREDIFFS
         for (i = 0; i < ARRLEN(__osPfsPifRam.ramarray) + 1; i++) { // also clear pifstatus
             __osPfsPifRam.ramarray[i] = 0xFFU;
         }
         __osPfsPifRam.pifstatus = CONT_CMD_REQUEST_STATUS;
+#endif
+
         ret = __osSiRawStartDma(OS_READ, &__osPfsPifRam);
         osRecvMesg(mq, NULL, OS_MESG_BLOCK);
-        ptr = (u8 *)&__osPfsPifRam;
-        if (channel != 0)
-            for (i = 0; i < channel; i++)
-                ptr++;
+        ptr = (u8*)&__osPfsPifRam;
 
-        ramreadformat = *(__OSContRamReadFormat *)ptr;
+        if (channel != 0) {
+            for (i = 0; i < channel; i++) {
+                ptr++;
+            }
+        }
+
+        ramreadformat = *READFORMAT(ptr);
 
         ret = CHNL_ERR(ramreadformat);
         if (ret == 0) {
             if (__osContDataCrc(buffer) != ramreadformat.datacrc) {
                 ret = __osPfsGetStatus(mq, channel);
+                
                 if (ret != 0) {
                     __osSiRelAccess();
                     return ret;
                 }
+                
                 ret = PFS_ERR_CONTRFAIL;
             }
+#ifndef RAREDIFFS
+        } else {
+            ret = PFS_ERR_NOPACK;
+#endif
         }
-        if (ret != PFS_ERR_CONTRFAIL)
-            break;
-    } while ((retry-- >= 0));
+    } while ((ret == PFS_ERR_CONTRFAIL) && retry-- >= 0);
+
     __osSiRelAccess();
 
     return ret;
@@ -64,9 +152,11 @@ static void __osPackRamWriteData(int channel, u16 address, u8 *buffer) {
 
     ptr = (u8 *)__osPfsPifRam.ramarray;
 
+#ifdef RAREDIFFS
     for (i = 0; i < ARRLEN(__osPfsPifRam.ramarray) + 1; i++) { // also clear pifstatus
         __osPfsPifRam.ramarray[i] = 0;
     }
+#endif
 
     __osPfsPifRam.pifstatus = CONT_CMD_EXE;
     ramreadformat.dummy = CONT_CMD_NOP;
@@ -75,18 +165,20 @@ static void __osPackRamWriteData(int channel, u16 address, u8 *buffer) {
     ramreadformat.cmd = CONT_CMD_WRITE_PAK;
     ramreadformat.address = (address << 0x5) | __osContAddressCrc(address);
     ramreadformat.datacrc = CONT_CMD_NOP;
-
+    
     for (i = 0; i < ARRLEN(ramreadformat.data); i++) {
         ramreadformat.data[i] = *buffer++;
     }
-
+    
     if (channel != 0) {
         for (i = 0; i < channel; i++) {
-            *ptr++ = 0;
+             *ptr++ = CONT_CMD_REQUEST_STATUS; 
         }
     }
-
+    
     *(__OSContRamReadFormat *)ptr = ramreadformat;
     ptr += sizeof(__OSContRamReadFormat);
     ptr[0] = CONT_CMD_END;
 }
+
+#endif
