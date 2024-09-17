0d202d636f47a1cbc3db920c8641f031f38a88e6
Ryan Myers <ryan.p.myers@gmail.com>
2024-09-16 21:52:20 -0400

    libc changes to be more in line with ultralib

--- src/lib/src/libc/rmonPrintf.c
+++ src/lib/src/libc/rmonPrintf.c
@@ -1,6 +1,3 @@
-/* The comment below is needed for this file to be picked up by generate_ld */
-/* RAM_POS: 0x800C9D40 */
-
 #include "rmonPrintf.h"
 #include "types.h"
 #include "macros.h"
@@ -10,7 +7,7 @@
 /**
  * This gets called inside of _PrintF, but it is just a fake pointer returned
  */
-static char *is_proutSyncPrintf(UNUSED char *str, UNUSED const char *buf, UNUSED size_t n) {
+static char *proutSyncPrintf(UNUSED char *str, UNUSED const char *buf, UNUSED size_t n) {
     return ((char *) 1);   /* return a fake pointer so that it's not NULL */
 }
 
@@ -23,6 +20,6 @@ static char *is_proutSyncPrintf(UNUSED char *str, UNUSED const char *buf, UNUSED
 void rmonPrintf(const char *format, ...) {
     va_list args;
     va_start(args, format);
-    _Printf(is_proutSyncPrintf, NULL, format, args);
+    _Printf(proutSyncPrintf, NULL, format, args);
     va_end(args);
 }
