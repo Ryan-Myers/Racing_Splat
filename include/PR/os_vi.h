11a1bfa33b0332cd6de5061a36e0d4b6b440e3ee
Ryan Myers <ryan.p.myers@gmail.com>
2024-09-19 20:30:32 -0400

    up to os_voice

--- include/PR/os_vi.h
+++ include/PR/os_vi.h
@@ -1,95 +1,101 @@
-#ifndef _ULTRA64_VI_H_
-#define _ULTRA64_VI_H_
 
-/* Ultra64 Video Interface */
+/*====================================================================
+ * os_vi.h
+ *
+ * Copyright 1995, Silicon Graphics, Inc.
+ * All Rights Reserved.
+ *
+ * This is UNPUBLISHED PROPRIETARY SOURCE CODE of Silicon Graphics,
+ * Inc.; the contents of this file may not be disclosed to third
+ * parties, copied or duplicated in any form, in whole or in part,
+ * without the prior written permission of Silicon Graphics, Inc.
+ *
+ * RESTRICTED RIGHTS LEGEND:
+ * Use, duplication or disclosure by the Government is subject to
+ * restrictions as set forth in subdivision (c)(1)(ii) of the Rights
+ * in Technical Data and Computer Software clause at DFARS
+ * 252.227-7013, and/or in similar or successor clauses in the FAR,
+ * DOD or NASA FAR Supplement. Unpublished - rights reserved under the
+ * Copyright Laws of the United States.
+ *====================================================================*/
 
+/*---------------------------------------------------------------------*
+        Copyright (C) 1998 Nintendo. (Originated by SGI)
+        
+        $RCSfile: os_vi.h,v $
+        $Revision: 1.1 $
+        $Date: 1998/10/09 08:01:20 $
+ *---------------------------------------------------------------------*/
+
+#ifndef _OS_VI_H_
+#define	_OS_VI_H_
+
+#ifdef _LANGUAGE_C_PLUS_PLUS
+extern "C" {
+#endif
+
+#include <PR/ultratypes.h>
+#include "os_thread.h"
+#include "os_message.h"
+
+
+#if defined(_LANGUAGE_C) || defined(_LANGUAGE_C_PLUS_PLUS)
+
+/**************************************************************************
+ *
+ * Type definitions
+ *
+ */
 
 /*
- * Video Interface (VI) special features
+ * Structure to store VI register values that remain the same between 2 fields
  */
-#define	OS_VI_GAMMA_ON			    0x0001
-#define	OS_VI_GAMMA_OFF			    0x0002
-#define	OS_VI_GAMMA_DITHER_ON		0x0004
-#define	OS_VI_GAMMA_DITHER_OFF		0x0008
-#define	OS_VI_DIVOT_ON			    0x0010
-#define	OS_VI_DIVOT_OFF			    0x0020
-#define	OS_VI_DITHER_FILTER_ON		0x0040
-#define	OS_VI_DITHER_FILTER_OFF		0x0080
-
-/*
- * Video Interface (VI) mode attribute bit
- */
-#define OS_VI_BIT_NONINTERLACE		0x0001          /* lo-res */
-#define OS_VI_BIT_INTERLACE		    0x0002          /* lo-res */
-#define OS_VI_BIT_NORMALINTERLACE	0x0004          /* hi-res */
-#define OS_VI_BIT_DEFLICKINTERLACE	0x0008          /* hi-res */
-#define OS_VI_BIT_ANTIALIAS		    0x0010
-#define OS_VI_BIT_POINTSAMPLE		0x0020
-#define OS_VI_BIT_16PIXEL		    0x0040
-#define OS_VI_BIT_32PIXEL		    0x0080
-#define OS_VI_BIT_LORES			    0x0100
-#define OS_VI_BIT_HIRES			    0x0200
-#define OS_VI_BIT_NTSC			    0x0400
-#define OS_VI_BIT_PAL			    0x0800
-
-
-/* Types */
-
-typedef struct
-{
-    u32 ctrl;
-    u32 width;
-    u32 burst;
-    u32 vSync;
-    u32 hSync;
-    u32 leap;
-    u32 hStart;
-    u32 xScale;
-    u32 vCurrent;
+typedef struct {
+    u32	ctrl;
+    u32	width;
+    u32	burst;
+    u32	vSync;
+    u32	hSync;
+    u32	leap;
+    u32	hStart;
+    u32	xScale;
+    u32	vCurrent;
 } OSViCommonRegs;
 
-typedef struct
-{
-    u32 origin;
-    u32 yScale;
-    u32 vStart;
-    u32 vBurst;
-    u32 vIntr;
+
+/*
+ * Structure to store VI register values that change between fields
+ */
+typedef struct {
+    u32	origin;
+    u32	yScale;
+    u32	vStart;	
+    u32	vBurst;
+    u32	vIntr;
 } OSViFieldRegs;
 
-typedef struct
-{
-    u8 type;
-    OSViCommonRegs comRegs;
-    OSViFieldRegs fldRegs[2];
+
+/*
+ * Structure for VI mode
+ */
+typedef struct {
+    u8			type;		/* Mode type */
+    OSViCommonRegs	comRegs;	/* Common registers for both fields */
+    OSViFieldRegs	fldRegs[2];	/* Registers for Field 1  & 2 */
 } OSViMode;
 
-typedef struct
-{
-    /* 0x00 */ u16 unk00; //some kind of flags.  swap buffer sets to 0x10
-    /* 0x02 */ u16 retraceCount;
-    /* 0x04 */ void* buffer;
-    /* 0x08 */ OSViMode *unk08;
-    /* 0x0c */ u32 features;
-    /* 0x10 */ OSMesgQueue *mq;
-    /* 0x14 */ OSMesg *msg;
-    /* 0x18 */ u32 unk18;
-    /* 0x1c */ u32 unk1c;
-    /* 0x20 */ u32 unk20;
-    /* 0x24 */ f32 unk24;
-    /* 0x28 */ u16 unk28;
-    /* 0x2c */ u32 unk2c;
-} OSViContext;
 
-void osCreateViManager(OSPri pri);
-void osViSetMode(OSViMode *mode);
-void osViSetEvent(OSMesgQueue *mq, OSMesg msg, u32 retraceCount);
-void osViBlack(u8 active);
-void osViSetSpecialFeatures(u32 func);
-void osViSwapBuffer(void *vaddr);
-void osYieldThread(void);
+#endif /* defined(_LANGUAGE_C) || defined(_LANGUAGE_C_PLUS_PLUS) */
 
+/**************************************************************************
+ *
+ * Global definitions
+ *
+ */
 
+/*
+ * Video Interface (VI) mode type
+ */
 #define OS_VI_NTSC_LPN1		0	/* NTSC */
 #define OS_VI_NTSC_LPF1		1
 #define OS_VI_NTSC_LAN1		2
@@ -134,6 +140,59 @@ void osYieldThread(void);
 #define OS_VI_MPAL_HAF1		39
 #define OS_VI_MPAL_HPN2		40
 #define OS_VI_MPAL_HPF2		41
+
+#define OS_VI_FPAL_LPN1         42      /* FPAL - Full screen PAL */
+#define OS_VI_FPAL_LPF1         43
+#define OS_VI_FPAL_LAN1         44
+#define OS_VI_FPAL_LAF1         45
+#define OS_VI_FPAL_LPN2         46
+#define OS_VI_FPAL_LPF2         47
+#define OS_VI_FPAL_LAN2         48
+#define OS_VI_FPAL_LAF2         49
+#define OS_VI_FPAL_HPN1         50
+#define OS_VI_FPAL_HPF1         51
+#define OS_VI_FPAL_HAN1         52
+#define OS_VI_FPAL_HAF1         53
+#define OS_VI_FPAL_HPN2         54
+#define OS_VI_FPAL_HPF2         55
+
+/*
+ * Video Interface (VI) special features
+ */
+#define	OS_VI_GAMMA_ON			0x0001
+#define	OS_VI_GAMMA_OFF			0x0002
+#define	OS_VI_GAMMA_DITHER_ON		0x0004
+#define	OS_VI_GAMMA_DITHER_OFF		0x0008
+#define	OS_VI_DIVOT_ON			0x0010
+#define	OS_VI_DIVOT_OFF			0x0020
+#define	OS_VI_DITHER_FILTER_ON		0x0040
+#define	OS_VI_DITHER_FILTER_OFF		0x0080
+
+/*
+ * Video Interface (VI) mode attribute bit
+ */
+#define OS_VI_BIT_NONINTERLACE		0x0001          /* lo-res */
+#define OS_VI_BIT_INTERLACE		0x0002          /* lo-res */
+#define OS_VI_BIT_NORMALINTERLACE	0x0004          /* hi-res */
+#define OS_VI_BIT_DEFLICKINTERLACE	0x0008          /* hi-res */
+#define OS_VI_BIT_ANTIALIAS		0x0010
+#define OS_VI_BIT_POINTSAMPLE		0x0020
+#define OS_VI_BIT_16PIXEL		0x0040
+#define OS_VI_BIT_32PIXEL		0x0080
+#define OS_VI_BIT_LORES			0x0100
+#define OS_VI_BIT_HIRES			0x0200
+#define OS_VI_BIT_NTSC			0x0400
+#define OS_VI_BIT_PAL			0x0800
+
+
+#if defined(_LANGUAGE_C) || defined(_LANGUAGE_C_PLUS_PLUS)
+
+/**************************************************************************
+ *
+ * Macro definitions
+ *
+ */
+
 
 /**************************************************************************
  *
@@ -203,7 +262,37 @@ extern OSViMode osViModeFpalHaf1;
 extern OSViMode osViModeFpalHpn2;
 extern OSViMode osViModeFpalHpf2;
 
-extern OSViMode	osViModeTable[];	/* Global VI mode table */
+
+/**************************************************************************
+ *
+ * Function prototypes
+ *
+ */
+
+/* Video interface (Vi) */
+extern u32		osViGetStatus(void);
+extern u32		osViGetCurrentMode(void);
+extern u32		osViGetCurrentLine(void);
+extern u32		osViGetCurrentField(void);
+extern void		*osViGetCurrentFramebuffer(void);
+extern void		*osViGetNextFramebuffer(void);
+extern void		osViSetXScale(f32);
+extern void		osViSetYScale(f32);
+extern void		osViExtendVStart(u32);
+extern void		osViSetSpecialFeatures(u32);
+extern void		osViSetMode(OSViMode *);
+extern void		osViSetEvent(OSMesgQueue *, OSMesg, u32);
+extern void		osViSwapBuffer(void *);
+extern void		osViBlack(u8);
+extern void		osViFade(u8, u16);
+extern void		osViRepeatLine(u8);
+extern void		osCreateViManager(OSPri);
 
 
+#endif  /* defined(_LANGUAGE_C) || defined(_LANGUAGE_C_PLUS_PLUS) */
+
+#ifdef _LANGUAGE_C_PLUS_PLUS
+}
 #endif
+
+#endif /* !_OS_VI_H_ */
