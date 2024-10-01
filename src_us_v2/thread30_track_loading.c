131e3341f1f44e25e937a9ce2d7135b77ef49211
Ryan Myers <ryan.p.myers@gmail.com>
2024-09-30 22:53:31 -0400

    Fix usv2 build

---
+++ src_us_v2/thread30_track_loading.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C78B0_C84B0.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C791C_C851C.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C7940_C8540.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C7950_C8550.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C79A8_C85A8.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C79B8_C85B8.s")
+
+#pragma GLOBAL_ASM("asm_us_v2/nonmatchings/thread30_track_loading/func_800C7A00_C8600.s")
