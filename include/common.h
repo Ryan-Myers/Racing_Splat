9e646e8b7566169412706fe1810b506d63257ee1
Ryan Myers <ryan.p.myers@gmail.com>
2024-10-03 13:04:07 -0400

    Add all of the data symbols for usv2

--- include/common.h
+++ include/common.h
@@ -10,6 +10,12 @@
 #define ViewportFuncChecksum 0x59E5
 #define TractionTableChecksum 0x1C38F
 #define ObjLoopGoldenBalloonChecksum 0xA6FF
+#elif defined(VERSION_us_v2)
+#define Func80019808Checksum 0x34B94
+#define ViewportFuncChecksum 0x5881
+#define TractionTableChecksum 0x1CA13
+#define ObjLoopGoldenBalloonChecksum 0xA745
+
 #else
 #define Func80019808Checksum 0x35281
 #define ViewportFuncChecksum 0x585E
