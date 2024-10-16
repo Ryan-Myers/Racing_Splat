5d737ec999b31fa78ba2c39e3f68a44c23cf6537
Ryan Myers <ryan.p.myers@gmail.com>
2024-10-16 12:03:03 -0400

    Many more jpn symbols

---
+++ src_jpn_v1/lib/src/io/motor.c
@@ -0,0 +1,9 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/io/motor/osMotorStop.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/io/motor/osMotorStart.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/io/motor/func_80072060_72C60.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/io/motor/osMotorInit.s")
--- src_jpn_v1/thread3_main.c
+++ src_jpn_v1/thread3_main.c
@@ -6,114 +6,114 @@
 
 #pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/main_game_loop.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006CBBC_6D7BC.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/load_next_ingame_level.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006CC30_6D830.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/load_level_game.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006CCEC_6D8EC.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/unload_level_game.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006CDC8_6D9C8.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/ingame_logic_loop.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006D97C_6E57C.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/set_drumstick_unlock_transition.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006D9B8_6E5B8.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/race_postrace_type.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006D9C8_6E5C8.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006D8F0.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DA40_6E640.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006D968.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DAE4_6E6E4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/get_game_mode.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DAF4_6E6F4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/set_game_mode.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DB00_6E700.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/load_menu_with_level_background.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DBEC_6E7EC.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/set_level_default_vehicle.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DBF8_6E7F8.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/set_vehicle_id_for_menu.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DC04_6E804.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/get_level_default_vehicle.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DC14_6E814.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/load_level_menu.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DCBC_6E8BC.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/unload_level_menu.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DD30_6E930.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/update_menu_scene.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006DDD0_6E9D0.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/menu_logic_loop.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006E3C0_6EFC0.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/load_level_for_menu.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006E494_6F094.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/calc_and_alloc_heap_for_settings.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006E694_6F294.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/init_racer_headers.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006E848_6F448.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/clear_lap_records.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EA6C_6F66C.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/clear_game_progress.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EB30_6F730.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/reset_save_data.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EB68_6F768.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/get_settings.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EB78_6F778.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/is_game_paused.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EB88_6F788.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/is_postrace_viewport_active.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EB98_6F798.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/is_reset_pressed.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EBEC_6F7EC.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/get_ingame_map_id.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EBFC_6F7FC.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_to_read_flap_times.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EC18_6F818.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_to_read_course_times.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EC34_6F834.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_to_read_flap_and_course_times.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EC50_6F850.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_read_save_file.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EC80_6F880.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_read_all_save_files.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EC9C_6F89C.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_to_write_flap_times.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006ECB8_6F8B8.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_to_write_course_times.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006ECD4_6F8D4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_to_write_flap_and_course_times.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006ECF0_6F8F0.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/force_mark_write_save_file.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006ED20_6F920.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/safe_mark_write_save_file.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006ED84_6F984.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_save_file_to_erase.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006ED9C_6F99C.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_read_eeprom_settings.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EDB8_6F9B8.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/mark_write_eeprom_settings.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006EDD4_6F9D4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/alloc_displaylist_heap.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F090_6FC90.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/check_dmem_validity.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F0B4_6FCB4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/default_alloc_displaylist_heap.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F218_6FE18.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/level_transition_begin.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F2E4_6FEE4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F20C.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F32C_6FF2C.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/begin_trophy_race_teleport.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F374_6FF74.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/begin_lighthouse_rocket_cutscene.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F410_70010.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/begin_level_teleport.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F460_70060.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/set_pause_lockout_timer.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F470_70070.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/swap_lead_player.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F504_70104.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/set_frame_blackout_timer.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F514_70114.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/pre_intro_loop.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F5A0_701A0.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/is_controller_missing.s")
 
-#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/func_8006F5C4_701C4.s")
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/thread3_main/check_imem_validity.s")
