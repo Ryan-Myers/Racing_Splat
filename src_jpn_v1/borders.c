0d70f62b81b38a519a0851295e1cd3016acf626e
Ryan Myers <ryan.p.myers@gmail.com>
2024-10-16 13:30:48 -0400

    Several more jpn symbols. Still builds.

---
+++ src_jpn_v1/borders.c
@@ -0,0 +1,5 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/borders/divider_draw.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/borders/divider_clear_coverage.s")
---
+++ src_jpn_v1/lib/src/sc/sched.c
@@ -0,0 +1,41 @@
+#include "common.h"
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/osCreateScheduler.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/osScAddClient.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/osScRemoveClient.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/osScGetCmdQ.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/osScGetInterruptQ.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/scGetAudioTaskTimers.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/func_800798BC_7A4BC.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/func_80079760.s")
+
+void dummy_80079808(void) {
+}
+
+void dummy_80079810(void) {
+}
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scHandleRetrace.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scHandleRSP.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scHandleRDP.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scTaskReady.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scTaskComplete.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scAppendList.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scExec.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/__scYield.s")
+
+#pragma GLOBAL_ASM("asm_jpn_v1/nonmatchings/lib/src/sc/sched/func_8007A3E4_7AFE4.s")
