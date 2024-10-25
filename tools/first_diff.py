152b35950fd5f52b3a9ea9837c73ae0f813423ab
Ryan Myers <ryan.p.myers@gmail.com>
2024-10-25 13:45:38 -0400

    Make the diff scripts default to usv1

--- tools/first_diff.py
+++ tools/first_diff.py
@@ -34,7 +34,7 @@ def firstDiffMain():
     parser = argparse.ArgumentParser(description="Find the first difference(s) between the built ROM and the base ROM.")
 
     parser.add_argument("-c", "--count", type=int, default=5, help="find up to this many instruction difference(s)")
-    parser.add_argument("-r", "--region", help="Which region should be processed", default="jpn")
+    parser.add_argument("-r", "--region", help="Which region should be processed", default="us")
     parser.add_argument("-v", "--version", help="Which version should be processed", default="v1")
     parser.add_argument("-a", "--add-colons", action='store_true', help="Add colon between bytes" )
 
